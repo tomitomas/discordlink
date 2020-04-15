a9a9bf9b486666072f41d54fc7c06c70b22fd078
Thibaut <30575266+trmaud@users.noreply.github.com>
2020-04-15 13:52:58 +0200

    Update discordlink.php

--- desktop/php/discordlink.php
+++ desktop/php/discordlink.php
@@ -55,7 +55,7 @@ foreach ($eqLogics as $eqLogic) {
     <form class="form-horizontal">
         <fieldset>
             <div class="form-group">
-                <label class="col-sm-3 control-label">{{Nom du channels}}</label>
+                <label class="col-sm-3 control-label">{{Nom du channel}}</label>
                 <div class="col-sm-3">
                     <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
                     <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de channels}}"/>
@@ -91,11 +91,11 @@ foreach ($eqLogics as $eqLogic) {
 		<div class="col-sm-9">
 			<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
 			<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
-            <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="interactionjeedom"/>{{Interation Avec Jeedom}}</label>
+            <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="interactionjeedom"/>{{Interations Avec Jeedom}}</label>
         </div>
 	</div>
        <div class="form-group">
-        <label class="col-sm-3 control-label">{{Channel : }}</label>
+        <label class="col-sm-3 control-label">{{Channels : }}</label>
         <div class="col-sm-3">
             <select class="form-control eqLogicAttr" data-l1key="configuration" data-l2key="channelid">       
                 <?php
