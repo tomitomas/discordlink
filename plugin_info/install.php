ad7c16089f1659060c0c71609c95290fd6022cfb
Thibaut <30575266+trmaud@users.noreply.github.com>
2020-04-25 21:09:04 +0200

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -20,12 +20,14 @@ require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 
 function discordlink_install() {
     discordlink::CreateCmd();
-    discordlink::dependancy_install(); 
+    $discordlink = plugin::byId('discordlink');
+    $discordlink->dependancy_install(); 
 }
 
 function discordlink_update() {
     discordlink::CreateCmd();
-    discordlink::dependancy_install();
+    $discordlink = plugin::byId('discordlink');
+    $discordlink->dependancy_install();
 }
 
 
