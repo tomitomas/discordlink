366d0ce9a63d2f7e6141e536cd3ea5b60a23a405
Thibaut <30575266+trmaud@users.noreply.github.com>
2020-05-06 17:34:24 +0200

    Update configuration.php

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -48,16 +48,6 @@ if (!isConnect()) {
                 ?>
             </div>
         </div>
-        </br>
-        <div class="form-group">
-            <label class="col-lg-4 control-label">{{Thème Icon}}</label>
-            <div class="col-lg-6">
-                <select id="sel_object" class="configKey form-control" data-l1key="themeIcon">
-                    <option value="1">Theme 1</option>
-                    <option value="2">Theme 2</option>
-                </select>
-            </div>
-        </div>
       <br/>
       <div class="form-group">
           <label class="col-lg-4 control-label">{{Joue à : }}</label>
