58ac6947408fa0ddafc4e84c82fecd74d62e9bb9
Thibaut <30575266+trmaud@users.noreply.github.com>
2020-05-07 02:03:35 +0200

    Update interface

--- desktop/php/discordlinkemote.php
+++ desktop/php/discordlinkemote.php
@@ -7,26 +7,16 @@ sendVarToJS('eqType', $plugin->getId());
 $eqLogics = eqLogic::byType($plugin->getId());
 ?>
 
-<div class="row row-overflow">
-   <div class="col-xs-12 eqLogicThumbnailDisplay">
-  <legend><i class="fas fa-cog"></i>  {{Gestion}}</legend>
-  <div class="eqLogicThumbnailContainer">
-    <div class="cursor eqLogicAction logoPrimary" data-action="add">
-        <i class="fas fa-plus-circle"></i>
-        <br>
-        <span>{{Ajouter un emojy}}</span>
-    </div>
-    <div class="cursor eqLogicAction logoSecondary" style="position: absolute; left: 0px; top: 0px;">
-        <a href="index.php?v=d&m=discordlink&p=discordlink"><img style="margin-top:-32px;" src="plugins/discordlink/plugin_info/discordlink_icon.png" width="75" height="75">
-        <br>
-        <span>DiscordLink</span></a>
-    </div>
+<div class="input-group" style="display:inline-flex">
+    <span class="input-group-btn">
+        <a class="btn btn-sm btn-success" href="index.php?v=d&m=discordlink&p=discordlink"><i class="fas fa-check-circle"></i>Retours à Discord Link</a>
+    </span>
 </div>
-
 <div class="input-group pull-right" style="display:inline-flex">
     <span class="input-group-btn">
         <a class="btn btn-sm btn-success eqLogicAction" data-action="saveemojy"><i class="fas fa-check-circle"></i> {{Sauvegarder}}</a>
         <a class="btn btn-success btn-sm cmdAction" id="bt_addemojy"><i class="fa fa-plus-circle"></i> Ajouter un Emojy</a>
+        <a class="btn btn-danger btn-sm cmdAction" id="bt_reset"><i class="fa fa-plus-circle"></i>Reset Emojy</a>
     </span>
 </div>
 
