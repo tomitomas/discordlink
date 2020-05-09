ef47e09abe68c279fabb639eb99cd31e310c1332
trmaud <30575266+trmaud@users.noreply.github.com>
2020-05-09 03:45:57 +0200

    Update discordlinkemote.php

--- desktop/php/discordlinkemote.php
+++ desktop/php/discordlinkemote.php
@@ -10,14 +10,14 @@ $eqLogics = eqLogic::byType($plugin->getId());
 <div id="div_AboAlert" style="display: none;"></div>
 <div class="input-group" style="display:inline-flex">
     <span class="input-group-btn">
-        <a class="btn btn-sm btn-success" href="index.php?v=d&m=discordlink&p=discordlink"><i class="fas fa-check-circle"></i>Retours à Discord Link</a>
+        <a class="btn btn-sm btn-success" href="index.php?v=d&m=discordlink&p=discordlink"><i class="fas fa-check-circle"></i>{{Retour à }} Discord Link</a>
     </span>
 </div>
 <div class="input-group pull-right" style="display:inline-flex">
     <span class="input-group-btn">
         <a class="btn btn-sm btn-success eqLogicAction" data-action="saveemojy"><i class="fas fa-check-circle"></i> {{Sauvegarder}}</a>
-        <a class="btn btn-success btn-sm cmdAction" id="bt_addemojy"><i class="fa fa-plus-circle"></i> Ajouter un Emojy</a>
-        <a class="btn btn-danger btn-sm cmdAction" id="bt_reset"><i class="fa fa-plus-circle"></i>Reset Emojy</a>
+        <a class="btn btn-success btn-sm cmdAction" id="bt_addemojy"><i class="fa fa-plus-circle"></i>{{Ajouter un émoji}}</a>
+        <a class="btn btn-danger btn-sm cmdAction" id="bt_reset"><i class="fa fa-plus-circle"></i>{{Reset émoji}}</a>
     </span>
 </div>
 
@@ -25,8 +25,8 @@ $eqLogics = eqLogic::byType($plugin->getId());
     <table id="table_cmd" class="table table-bordered table-condensed ui-sortable">
         <thead>
         <tr class="emojy">
-            <th style="">Clée emojy</th>
-            <th style="">Code Emojy</th>
+            <th style="">{{Clé émoji}}</th>
+            <th style="">{{Code émoji}}</th>
             <th style="width: 100px;"></th>
         </tr>
         </thead>
