440bae0848d77e87ca0832f73b7d0eea3ee03d94
Thibaut <30575266+trmaud@users.noreply.github.com>
2020-06-02 15:20:18 +0200

    Update jeediscordlink.php

--- core/php/jeediscordlink.php
+++ core/php/jeediscordlink.php
@@ -92,7 +92,7 @@ function getdevicepuisupdate($nom, $variable, $commandejeedom, $_idchannel, $id_
 	if ($discordlinkeqlogic->getConfiguration('interactionjeedom') == 1) {
 		$parameters['plugin'] = 'discordlink';
 		$parameters['userid'] = $id_user;
-		$parameters['channel'] = $_idchannel
+		$parameters['channel'] = $_idchannel;
 		$reply = interactQuery::tryToReply(trim($variable), $parameters);
 		log::add('discordlink', 'debug', 'Interaction ' . print_r($reply, true));
 		if ($reply['reply'] != "Désolé je n'ai pas compris" && $reply['reply'] != "Désolé je n'ai pas compris la demande" && $reply['reply'] != "Désolé je ne comprends pas la demande" && $reply['reply'] != "Je ne comprends pas" && $reply['reply'] != "ceci est un message de test" && $reply['reply'] != "" && $reply['reply'] != " ") {
