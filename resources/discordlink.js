2e5e1320e10a12cf2ed0a646139c2015130e0922
Thibaut Trarbach <trmaud@gmail.com>
2021-04-15 10:00:33 +0200

    - Mise a jours de la commande clear Message

--- resources/discordlink.js
+++ resources/discordlink.js
@@ -8,46 +8,44 @@ const request = require('request');
 
 const token = process.argv[3];
 const IPJeedom = process.argv[2];
-const ClePlugin =  process.argv[6];
+const ClePlugin = process.argv[6];
 const joueA = decodeURI(process.argv[7]);
 
 
 /* Configuration */
 const config = {
-	logger: console2,
-	token: token,
-	listeningPort: 3466
+    logger: console2,
+    token: token,
+    listeningPort: 3466
 };
 
 let dernierStartServeur = 0;
 
 if (!token) config.logger('DiscordLink-Config: *********************TOKEN NON DEFINI*********************');
 
-function console2(text, level='') {
-	try {
-		let niveauLevel;
-		switch (level) {
-		case "ERROR":
-				niveauLevel=400;
-				break;
-		case "WARNING":
-				niveauLevel=300;
-				break;
-		case "INFO":
-				niveauLevel=200;
-				break;
-		case "DEBUG":
-				niveauLevel=100;
-				break;
-		default:
-				niveauLevel=400; //pour trouver ce qui n'a pas été affecté à un niveau
-				break;
-		}
-	} catch (e) {
-		console.log(arguments[0]);
-	}
-
-
+function console2(text, level = '') {
+    try {
+        let niveauLevel;
+        switch (level) {
+            case "ERROR":
+                niveauLevel = 400;
+                break;
+            case "WARNING":
+                niveauLevel = 300;
+                break;
+            case "INFO":
+                niveauLevel = 200;
+                break;
+            case "DEBUG":
+                niveauLevel = 100;
+                break;
+            default:
+                niveauLevel = 400; //pour trouver ce qui n'a pas été affecté à un niveau
+                break;
+        }
+    } catch (e) {
+        console.log(arguments[0]);
+    }
 }
 
 /* Routing */
@@ -56,30 +54,30 @@ let server = null;
 
 /***** Stop the server *****/
 app.get('/stop', (req, res) => {
-	config.logger('DiscordLink: Shutting down');
-	res.status(200).json({});
-	server.close(() => {
-		process.exit(0);
-	});
+    config.logger('DiscordLink: Shutting down');
+    res.status(200).json({});
+    server.close(() => {
+        process.exit(0);
+    });
 });
 
 /***** Restart server *****/
 app.get('/restart', (req, res) => {
-	config.logger('DiscordLink: Restart');
-	res.status(200).json({});
-		config.logger('DiscordLink: ******************************************************************');
-		config.logger('DiscordLink: *****************************Relance forcée du Serveur*************');
-		config.logger('DiscordLink: ******************************************************************');
-	startServer();
+    config.logger('DiscordLink: Restart');
+    res.status(200).json({});
+    config.logger('DiscordLink: ******************************************************************');
+    config.logger('DiscordLink: *****************************Relance forcée du Serveur*************');
+    config.logger('DiscordLink: ******************************************************************');
+    startServer();
 
 });
 
 app.get('/getinvite', (req, res) => {
 
     res.type('json');
-	let toReturn = [];
+    let toReturn = [];
 
-	config.logger('DiscordLink: GetInvite');
+    config.logger('DiscordLink: GetInvite');
     client.generateInvite(["ADMINISTRATOR"]).then(link => {
         toReturn.push({
             'invite': link
@@ -90,18 +88,18 @@ app.get('/getinvite', (req, res) => {
 
 app.get('/getchannel', (req, res) => {
     res.type('json');
-	let toReturn = [];
+    let toReturn = [];
 
-	config.logger('DiscordLink: GetChannel');
-	let channelsall = client.channels.cache.array();
+    config.logger('DiscordLink: GetChannel');
+    let channelsall = client.channels.cache.array();
     for (let b in channelsall) {
-		let channel = channelsall[b];
+        let channel = channelsall[b];
         if (channel.type === "text") {
             toReturn.push({
                 'id': channel.id,
                 'name': channel.name,
-				'guildID': channel.guild.id,
-				'guildName' : channel.guild.name
+                'guildID': channel.guild.id,
+                'guildName': channel.guild.name
             });
         }
     }
@@ -110,11 +108,11 @@ app.get('/getchannel', (req, res) => {
 
 app.get('/sendMsg', (req, res) => {
     res.type('json');
-	let toReturn = [];
+    let toReturn = [];
 
-	config.logger('DiscordLink: sendMsg');
-	
-	toReturn.push({
+    config.logger('DiscordLink: sendMsg');
+
+    toReturn.push({
         'id': req.query
     });
     res.status(200).json(toReturn);
@@ -124,16 +122,16 @@ app.get('/sendMsg', (req, res) => {
 
 app.get('/sendFile', (req, res) => {
     res.type('json');
-	let toReturn = [];
+    let toReturn = [];
 
     config.logger('DiscordLink: sendMsg');
 
     client.channels.cache.get(req.query.channelID).send(req.query.message, {
-		files: [{
-		  attachment: req.query.patch,
-		  name: req.query.name
-		}]
-	});
+        files: [{
+            attachment: req.query.patch,
+            name: req.query.name
+        }]
+    });
 
     toReturn.push({
         'id': req.query
@@ -143,13 +141,13 @@ app.get('/sendFile', (req, res) => {
 
 app.get('/sendMsgTTS', (req, res) => {
     res.type('json');
-	let toReturn = [];
+    let toReturn = [];
 
-	config.logger('DiscordLink: sendMsgTTS');
+    config.logger('DiscordLink: sendMsgTTS');
 
     client.channels.cache.get(req.query.channelID).send(req.query.message, {
         tts: true
-       });
+    });
 
     toReturn.push({
         'id': req.query
@@ -159,203 +157,207 @@ app.get('/sendMsgTTS', (req, res) => {
 
 app.get('/sendEmbed', (req, res) => {
     res.type('json');
-	let toReturn = [];
+    let toReturn = [];
 
-	config.logger('DiscordLink: sendEmbed');
+    config.logger('DiscordLink: sendEmbed');
 
-	let color = req.query.color;
-	let title = req.query.title;
-	let url = req.query.url;
-	let description = req.query.description;
-	let countanswer = req.query.countanswer;
-	let fields = req.query.field;
-	let footer = req.query.footer;
-	let reponse = "null";
+    let color = req.query.color;
+    let title = req.query.title;
+    let url = req.query.url;
+    let description = req.query.description;
+    let countanswer = req.query.countanswer;
+    let fields = req.query.field;
+    let footer = req.query.footer;
+    let reponse = "null";
 
-	if (color ==="null")color = "#ff0000";
+    if (color === "null") color = "#ff0000";
 
-	const Embed = new Discord.MessageEmbed()
-	.setColor(color)
-	.setTimestamp();
-	if(title !== "null") Embed.setTitle(title);
-	if(url !== "null" && countanswer === "null") Embed.setURL(url);
-	if(description !== "null") Embed.setDescription(description);
-	if(footer !== "null") Embed.setFooter(footer);
+    const Embed = new Discord.MessageEmbed()
+        .setColor(color)
+        .setTimestamp();
+    if (title !== "null") Embed.setTitle(title);
+    if (url !== "null" && countanswer === "null") Embed.setURL(url);
+    if (description !== "null") Embed.setDescription(description);
+    if (footer !== "null") Embed.setFooter(footer);
 
-	if (fields !== "null") {
-		fields = JSON.parse(fields);
-		for(let field in fields){
-			let name = fields[field]['name'];
-			let value = fields[field]['value'];
-			let inline = fields[field]['inline'];
+    if (fields !== "null") {
+        fields = JSON.parse(fields);
+        for (let field in fields) {
+            let name = fields[field]['name'];
+            let value = fields[field]['value'];
+            let inline = fields[field]['inline'];
 
-			inline = inline === 1;
+            inline = inline === 1;
 
 
-			console.log(fields[field])
-			console.log("Name : "+name+" | Value : "+value)
+            console.log(fields[field])
+            console.log("Name : " + name + " | Value : " + value)
 
-			Embed.addField(name, value, inline)
-		}
-	}
+            Embed.addField(name, value, inline)
+        }
+    }
     client.channels.cache.get(req.query.channelID).send(Embed).then(async m => {
-		if (countanswer !== "null") {
+        if (countanswer !== "null") {
 
-			let timecalcul = (req.query.timeout * 1000);
-			toReturn.push({
-				'querry': req.query,
-				'timeout': req.query.timeout,
-				'timecalcul': timecalcul
-			});
-			res.status(200).json(toReturn);
+            let timecalcul = (req.query.timeout * 1000);
+            toReturn.push({
+                'querry': req.query,
+                'timeout': req.query.timeout,
+                'timecalcul': timecalcul
+            });
+            res.status(200).json(toReturn);
 
-			let emojy = ["🇦", "🇧", "🇨", "🇩", "🇪", "🇫", "🇬", "🇭", "🇮", "🇯", "🇰", "🇱", "🇲", "🇳", "🇴", "🇵", "🇶", "🇷", "🇸", "🇹", "🇺", "🇻", "🇼", "🇽", "🇾", "🇿"];
-			let a = 0;
-			while (a < countanswer) {
-				await m.react(emojy[a]);
-				a++;
-			}
-			const filter = (reaction, user) => {
-				return ["🇦", "🇧", "🇨", "🇩", "🇪", "🇫", "🇬", "🇭", "🇮", "🇯", "🇰", "🇱", "🇲", "🇳", "🇴", "🇵", "🇶", "🇷", "🇸", "🇹", "🇺", "🇻", "🇼", "🇽", "🇾", "🇿"].includes(reaction.emoji.name) && user.id !== m.author.id;
-			};
-
-
-			m.awaitReactions(filter, {max: 1, time: timecalcul, errors: ['time']})
-				.then(collected => {
-					const reaction = collected.first();
-					if (reaction.emoji.name === '🇦') reponse = 0;
-					else if (reaction.emoji.name === '🇧') reponse = 1;
-					else if (reaction.emoji.name === '🇨') reponse = 2;
-					else if (reaction.emoji.name === '🇩') reponse = 3;
-					else if (reaction.emoji.name === '🇪') reponse = 4;
-					else if (reaction.emoji.name === '🇫') reponse = 5;
-					else if (reaction.emoji.name === '🇬') reponse = 6;
-					else if (reaction.emoji.name === '🇭') reponse = 7;
-					else if (reaction.emoji.name === '🇮') reponse = 8;
-					else if (reaction.emoji.name === '🇯') reponse = 9;
-					else if (reaction.emoji.name === '🇰') reponse = 10;
-					else if (reaction.emoji.name === '🇱') reponse = 11;
-					else if (reaction.emoji.name === '🇲') reponse = 12;
-					else if (reaction.emoji.name === '🇳') reponse = 13;
-					else if (reaction.emoji.name === '🇴') reponse = 14;
-					else if (reaction.emoji.name === '🇵') reponse = 15;
-					else if (reaction.emoji.name === '🇶') reponse = 16;
-					else if (reaction.emoji.name === '🇷') reponse = 17;
-					else if (reaction.emoji.name === '🇸') reponse = 18;
-					else if (reaction.emoji.name === '🇹') reponse = 19;
-					else if (reaction.emoji.name === '🇺') reponse = 20;
-					else if (reaction.emoji.name === '🇻') reponse = 21;
-					else if (reaction.emoji.name === '🇼') reponse = 22;
-					else if (reaction.emoji.name === '🇽') reponse = 23;
-					else if (reaction.emoji.name === '🇾') reponse = 24;
-					else if (reaction.emoji.name === '🇿') reponse = 25;
-
-					url = JSON.parse(url);
+            let emojy = ["🇦", "🇧", "🇨", "🇩", "🇪", "🇫", "🇬", "🇭", "🇮", "🇯", "🇰", "🇱", "🇲", "🇳", "🇴", "🇵", "🇶", "🇷", "🇸", "🇹", "🇺", "🇻", "🇼", "🇽", "🇾", "🇿"];
+            let a = 0;
+            while (a < countanswer) {
+                await m.react(emojy[a]);
+                a++;
+            }
+            const filter = (reaction, user) => {
+                return ["🇦", "🇧", "🇨", "🇩", "🇪", "🇫", "🇬", "🇭", "🇮", "🇯", "🇰", "🇱", "🇲", "🇳", "🇴", "🇵", "🇶", "🇷", "🇸", "🇹", "🇺", "🇻", "🇼", "🇽", "🇾", "🇿"].includes(reaction.emoji.name) && user.id !== m.author.id;
+            };
 
 
-					httpPost("ASK", {
-						idchannel: m.channel.id,
-						reponse: reponse,
-						demande: url
-					});
+            m.awaitReactions(filter, {max: 1, time: timecalcul, errors: ['time']})
+                .then(collected => {
+                    const reaction = collected.first();
+                    if (reaction.emoji.name === '🇦') reponse = 0;
+                    else if (reaction.emoji.name === '🇧') reponse = 1;
+                    else if (reaction.emoji.name === '🇨') reponse = 2;
+                    else if (reaction.emoji.name === '🇩') reponse = 3;
+                    else if (reaction.emoji.name === '🇪') reponse = 4;
+                    else if (reaction.emoji.name === '🇫') reponse = 5;
+                    else if (reaction.emoji.name === '🇬') reponse = 6;
+                    else if (reaction.emoji.name === '🇭') reponse = 7;
+                    else if (reaction.emoji.name === '🇮') reponse = 8;
+                    else if (reaction.emoji.name === '🇯') reponse = 9;
+                    else if (reaction.emoji.name === '🇰') reponse = 10;
+                    else if (reaction.emoji.name === '🇱') reponse = 11;
+                    else if (reaction.emoji.name === '🇲') reponse = 12;
+                    else if (reaction.emoji.name === '🇳') reponse = 13;
+                    else if (reaction.emoji.name === '🇴') reponse = 14;
+                    else if (reaction.emoji.name === '🇵') reponse = 15;
+                    else if (reaction.emoji.name === '🇶') reponse = 16;
+                    else if (reaction.emoji.name === '🇷') reponse = 17;
+                    else if (reaction.emoji.name === '🇸') reponse = 18;
+                    else if (reaction.emoji.name === '🇹') reponse = 19;
+                    else if (reaction.emoji.name === '🇺') reponse = 20;
+                    else if (reaction.emoji.name === '🇻') reponse = 21;
+                    else if (reaction.emoji.name === '🇼') reponse = 22;
+                    else if (reaction.emoji.name === '🇽') reponse = 23;
+                    else if (reaction.emoji.name === '🇾') reponse = 24;
+                    else if (reaction.emoji.name === '🇿') reponse = 25;
 
-				})
-				.catch(() => {
-					m.delete();
-				});
-		}
-	}).catch(console.error);
-	if(countanswer === "null") {
-		toReturn.push({
-			'querry': req.query
-		});
-		res.status(200).json(toReturn);
-	}
+                    url = JSON.parse(url);
+
+                    httpPost("ASK", {
+                        idchannel: m.channel.id,
+                        reponse: reponse,
+                        demande: url
+                    });
+                })
+                .catch(() => {
+                    m.delete();
+                });
+        }
+    }).catch(console.error);
+    if (countanswer === "null") {
+        toReturn.push({
+            'querry': req.query
+        });
+        res.status(200).json(toReturn);
+    }
 });
 
 async function deletemessagechannel(message) {
-	let date = new Date();
-	let timestamp = date.getTime();
-	let mindaytimestamp = timestamp - 172800000;
-	let maxdaytimestamp = timestamp - 1206000000;
-	const fetched = await message.channel.messages.fetch({force:true});
-	const delmessage = [];
-
-	for (const message of fetched) {
-		if (maxdaytimestamp < message[1].createdTimestamp && message[1].createdTimestamp <= mindaytimestamp) {
-			if (message[1].deletable) {
-				delmessage.push(message[1])
-			}
-		}
-	}
-	await message.channel.bulkDelete(delmessage);
+    let date = new Date();
+    let timestamp = date.getTime();
+    let mindaytimestamp = timestamp - 172800000;
+    let maxdaytimestamp = timestamp - 1206000000;
+    let allDelete = true;
+    while (allDelete) {
+        const fetched = await message.channel.messages.fetch({force: true, limit: 100});
+        const delmessage = [];
+        for (const message of fetched) {
+            if (maxdaytimestamp < message[1].createdTimestamp /*&& message[1].createdTimestamp <= mindaytimestamp*/) {
+                if (message[1].deletable) {
+                    delmessage.push(message[1])
+                }
+            }
+        }
+        if (delmessage.length === 0) {
+            allDelete = false;
+        } else {
+            await message.channel.bulkDelete(delmessage);
+        }
+    }
 }
+
 /* Main */
 
 startServer();
 
 function startServer() {
-    dernierStartServeur=Date.now();
+    dernierStartServeur = Date.now();
 
-    config.logger('DiscordLink:    ******************** Lancement BOT ***********************','INFO');
+    config.logger('DiscordLink:    ******************** Lancement BOT ***********************', 'INFO');
 
     client.login(config.token);
 
     server = app.listen(config.listeningPort, () => {
-        config.logger('DiscordLink:    **************************************************************','INFO');
-        config.logger('DiscordLink:    ************** Server OK listening on port ' + server.address().port + ' **************','INFO');
-        config.logger('DiscordLink:    **************************************************************','INFO');
+        config.logger('DiscordLink:    **************************************************************', 'INFO');
+        config.logger('DiscordLink:    ************** Server OK listening on port ' + server.address().port + ' **************', 'INFO');
+        config.logger('DiscordLink:    **************************************************************', 'INFO');
 
     });
 }
 
 function httpPost(nom, jsonaenvoyer) {
 
-	let url = IPJeedom + "/plugins/discordlink/core/php/jeediscordlink.php?apikey=" + ClePlugin + "&nom=" + nom;
+    let url = IPJeedom + "/plugins/discordlink/core/php/jeediscordlink.php?apikey=" + ClePlugin + "&nom=" + nom;
 
-	config.logger && config.logger('URL envoyée: '+url, "DEBUG");
+    config.logger && config.logger('URL envoyée: ' + url, "DEBUG");
 
-	jsonaenvoyer=JSON.stringify(jsonaenvoyer);
-	config.logger && config.logger('DATA envoyé:'+jsonaenvoyer,'DEBUG');
+    jsonaenvoyer = JSON.stringify(jsonaenvoyer);
+    config.logger && config.logger('DATA envoyé:' + jsonaenvoyer, 'DEBUG');
 
-	request.post(url, {
-		json : true,
-		gzip : false,
-		multipart: [
-			{
-			body: jsonaenvoyer
-			}
-		]
-	}, function (err, response, json) {
+    request.post(url, {
+        json: true,
+        gzip: false,
+        multipart: [
+            {
+                body: jsonaenvoyer
+            }
+        ]
+    }, function (err, response, json) {
 
-		if (!err && response.statusCode === 200) {
+        if (!err && response.statusCode === 200) {
 
-		} else
-		{
+        } else {
 
-		}
-	});
+        }
+    });
 }
 
-client.on("ready", async() => {
+client.on("ready", async () => {
     await client.user.setActivity(joueA);
 });
 
 client.on('message', (receivedMessage) => {
 
-    if (receivedMessage.author === client.user) {
-    	if (receivedMessage.content === "!clearmessagechannel") {
-			deletemessagechannel(receivedMessage);
-			receivedMessage.delete();
-		}
+
+    if (receivedMessage.content === "!clearmessagechannel") {
+        deletemessagechannel(receivedMessage);
+        receivedMessage.delete();
         return;
-	}
-	if (receivedMessage.author.bot) {
-		return;
-	}
-	httpPost("messagerecu",{
-		idchannel: receivedMessage.channel.id,
-		message: receivedMessage.content,
-		iduser: receivedMessage.author.id
-	});
+    }
+    if (receivedMessage.author === client.user) return;
+    if (receivedMessage.author.bot) return;
+
+    httpPost("messagerecu", {
+        idchannel: receivedMessage.channel.id,
+        message: receivedMessage.content,
+        iduser: receivedMessage.author.id
+    });
+
 });
